d4691cc747382114748b01268ef751c631f741bc
LeDucLoi193 <dekaito193@gmail.com>
2022-12-26 23:42:40 +0700

    Add linting & format code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    minimumCacheTTL: 10368000,
+  },
+  poweredByHeader: false,
   reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  env: {
+    APP_ENV: process.env.APP_ENV || 'development',
+  },
+  webpack: (config) => {
+    config.plugins = config.plugins.filter((plugin) => plugin.constructor.name !== 'ForkTsCheckerWebpackPlugin')
+
+    config.watchOptions = {
+      aggregateTimeout: 300,
+      poll: 5000,
+      ignored: ['**/.git', '**/.next', '**/node_modules'],
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
